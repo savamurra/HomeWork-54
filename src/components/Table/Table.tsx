4e528242460bb54ab0b752445e72391679b36b12
Sanzhar Beishekeev <franki-55@mail.ru>
2024-09-23 16:36:39 +0600

    Создал компонент Table для прорисовки ячеек

---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,32 @@
+
+import { IProps } from '../../types';
+import * as React from 'react';
+import Cell from '../Cell/Cell.tsx';
+
+interface TableProps {
+    items: IProps[];
+    cellClicked: (id: string) => void;
+}
+
+const Table: React.FC<TableProps> = ({ items, cellClicked }) => {
+    return (
+        <div
+            style={{
+                display: 'grid',
+                gridTemplateColumns: 'repeat(6, 50px)',
+                gap: '4px',
+                border: '1px solid black',
+            }}
+        >
+            {items.map((item) => (
+                <Cell
+                    key={item.id}
+                    item={item}
+                    cellClicked={() => cellClicked(item.id)}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default Table;
