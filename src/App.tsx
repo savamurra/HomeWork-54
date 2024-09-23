b8264904d324e74124d4f6b50955106043867271
Sanzhar Beishekeev <franki-55@mail.ru>
2024-09-23 16:38:56 +0600

    В App создал функцию которая гененирует массив из 36 обьектов а также рендерит доску с ячейками

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
+
 import './App.css';
+import { useState } from 'react';
+import { IProps } from './types';
+import Table from './components/Table/Table.tsx';
 
 const App = () => {
+    const createCell = (): IProps[] => {
+        const cellArray: IProps[] = [];
+
+        for (let i = 0; i < 36; i++) {
+            cellArray.push({ hasItem: false, clicked: false, id: crypto.randomUUID() });
+        }
+
+        const randomCell = Math.floor(Math.random() * cellArray.length);
+        cellArray[randomCell].hasItem = true;
+
+        return cellArray;
+    };
+
+    const [items, setItems] = useState(createCell());
+
+    const cellClick = (id: string) => {
+        setItems((prevItems) =>
+            prevItems.map((item) =>
+                item.id === id ? { ...item, clicked: true } : item
+            )
+        );
+    };
+
     return (
         <>
+            <Table items={items} cellClicked={cellClick} />
         </>
     );
 };
-
 
 export default App;
